7b00b6117db46ff7cb0e7e008bda87afec2a3868
Dionisio Vieira <dionisioviei@gmail.com>
2023-05-11 18:58:02 -0300

    add type to dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: "src/index.ts",
